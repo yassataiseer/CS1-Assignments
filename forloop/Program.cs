9e442dfd7147ff6546ca38f8826c7d61f3abff21
Yassa Taiseer <yassataiseer@gmail.com>
2021-03-03 18:29:30 -0500

    commit on main

---
+++ forloop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Multiplication table generator");
+            Console.Write("Input the number of which will generate a multiplication table: ");
+            int Table = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < 11; i++)
+            {
+                int Answer = i * Table;
+                Console.WriteLine("{0}x{1} = {2}",i,Table,Answer);
+            }
+        }
+    }
+}

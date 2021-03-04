d7521a303b5eab3f16738c28f4d557c40587b9b3
Yassa Taiseer <yassataiseer@gmail.com>
2021-03-04 08:59:25 -0500

    commit on main

---
+++ ShapeMaker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ShapeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a triangle: ");
+            string Design = "";
+            for (int i = 0; i < 10; i++)
+            {
+                Design += "*";
+                Console.WriteLine(Design);
+            }
+            Console.WriteLine("This is a square: ");
+            string SquareDesign = "";
+            for (int r = 0; r<5;r++)
+            {
+                for (int j = 0; j<5;j++)
+                {
+                    SquareDesign += "*";
+                }
+                Console.WriteLine(SquareDesign);
+                SquareDesign = "";
+            }
+
+            Console.WriteLine("This is a rectangle: ");
+            string Rectangle = "";
+            for (int length = 0; length < 6; length++)
+            {
+                for (int width = 0; width < 3; width++)
+                {
+                    Rectangle += "*";
+                }
+                Console.WriteLine(Rectangle);
+                Rectangle = "";
+            }
+        }
+    }
+}

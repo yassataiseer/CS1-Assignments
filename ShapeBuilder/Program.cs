48157da70eaf207b17e9d43048e6061623104991
Yassa Taiseer <yassataiseer@gmail.com>
2021-03-15 11:27:54 -0400

    commit on main

---
+++ ShapeBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ShapeBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Length = Convert.ToInt16(Console.ReadLine());
+            int Width = Convert.ToInt16(Console.ReadLine());
+            for (int r = 0; r < Length; r++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    SquareDesign += "*";
+                }
+                Console.WriteLine(SquareDesign);
+                SquareDesign = "";
+            }
+        }
+    }
+}
